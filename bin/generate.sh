7032fb6436239cac1e5979920a825d1dde2ae66d
Kevin Lutzer <kevinlutzer@nureva.com>
2024-08-23 12:00:51 -0400

    update generate script

--- bin/generate.sh
+++ bin/generate.sh
@@ -6,5 +6,5 @@ gzip -k -f Packages
 
 # Release, Release.gpg & InRelease
 apt-ftparchive release . > Release
-gpg2 --default-key 8FEEDF80F3484CFE99E3BD1D416932846F1D3718 -abs -o - Release > Release.gpg
-gpg2 --default-key 8FEEDF80F3484CFE99E3BD1D416932846F1D3718 --clearsign -o - Release > InRelease
+gpg2 --default-key "gpg-github-ppa@nureva.com" -abs -o - Release > Release.gpg
+gpg2 --default-key "gpg-github-ppa@nureva.com" --clearsign -o - Release > InRelease
